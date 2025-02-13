7e0e0ba070fb6f437470a678008e19227fd5f03b
Cameron2001 <CameronBryson2001@protonmail.com>
2025-02-12 22:04:25 -0500

    Texture cubemap using fbo

--- src/Engine/Platform/OpenGL/OpenGLTextureCubemap.cpp
+++ src/Engine/Platform/OpenGL/OpenGLTextureCubemap.cpp
@@ -1,4 +1,4 @@
-#include "pch.hpp"
+#include "pch.hpp" 
 #include "OpenGLTextureCubemap.hpp"
 
 #include <stb_image.h>
@@ -7,6 +7,9 @@
 #include <iostream>
 #include "Engine/Graphics/Abstract/Shader.hpp"
 #include "OpenGLUtil.hpp"  
+
+// Include the abstract FrameBuffer interface
+#include <FrameBuffer.hpp>
 
 // Resolution (width and height) of each cubemap face.
 static const unsigned int ENV_MAP_SIZE = 512;
@@ -31,11 +34,9 @@ static glm::mat4 captureViews[] =
     glm::lookAt(glm::vec3(0.0f), glm::vec3(0.0f,  0.0f, -1.0f),  glm::vec3(0.0f, -1.0f,  0.0f))
 };
 
-
 OpenGLTextureCubemap::OpenGLTextureCubemap(const std::string& hdrPath,
     const std::shared_ptr<Shader>& equirectShader)
 {
-    // 1) Load HDR equirectangular data
     stbi_set_flip_vertically_on_load(true);
     int w, h, nrComponents;
     float* hdrData = stbi_loadf(hdrPath.c_str(), &w, &h, &nrComponents, 0);
@@ -45,7 +46,6 @@ OpenGLTextureCubemap::OpenGLTextureCubemap(const std::string& hdrPath,
         return;
     }
 
-    // Create a floating point 2D texture for the HDR data.
     GLuint hdrTexID;
     glGenTextures(1, &hdrTexID);
     glBindTexture(GL_TEXTURE_2D, hdrTexID);
@@ -57,12 +57,10 @@ OpenGLTextureCubemap::OpenGLTextureCubemap(const std::string& hdrPath,
 
     stbi_image_free(hdrData);
 
-    // 2) Create an empty cubemap texture
     glGenTextures(1, &textureID);
     glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
     for (unsigned int i = 0; i < 6; ++i)
     {
-        // Allocate storage for each face.
         glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F,
             ENV_MAP_SIZE, ENV_MAP_SIZE, 0, GL_RGB, GL_FLOAT, nullptr);
     }
@@ -75,14 +73,10 @@ OpenGLTextureCubemap::OpenGLTextureCubemap(const std::string& hdrPath,
     width = ENV_MAP_SIZE;
     height = ENV_MAP_SIZE;
 
-    // 3) Set up framebuffer and renderbuffer for capturing cubemap faces.
-    GLuint captureFBO, captureRBO;
-    glGenFramebuffers(1, &captureFBO);
-    glGenRenderbuffers(1, &captureRBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
-    glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, ENV_MAP_SIZE, ENV_MAP_SIZE);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, captureRBO);
+    auto fbo = FrameBuffer::createFrameBuffer(
+        ENV_MAP_SIZE, ENV_MAP_SIZE,
+        { { FrameBufferAttachmentType::Depth, FrameBufferTextureFormat::Depth24 } }
+    );
 
     // 4) Configure the shader and bind the HDR texture.
     equirectShader->use();
@@ -91,25 +85,24 @@ OpenGLTextureCubemap::OpenGLTextureCubemap(const std::string& hdrPath,
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, hdrTexID);
 
-    // 5) Convert the HDR equirectangular texture to a cubemap texture.
     glViewport(0, 0, ENV_MAP_SIZE, ENV_MAP_SIZE);
-    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
+    fbo->bind();
     for (unsigned int i = 0; i < 6; ++i)
     {
         equirectShader->setMat4("view", captureViews[i]);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-            GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, textureID, 0);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Render a cube. This utility function should render a unit cube.
+        fbo->attachExternalTexture(
+            GL_COLOR_ATTACHMENT0,
+            GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+            textureID,
+            0
+        );
+		fbo->setDrawBuffers({ GL_COLOR_ATTACHMENT0 });
+        fbo->clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         OpenGlUtil::drawCube();
     }
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    fbo->unbind();
 
-    // 6) Clean up the temporary HDR texture and framebuffer objects.
     glDeleteTextures(1, &hdrTexID);
-    glDeleteFramebuffers(1, &captureFBO);
-    glDeleteRenderbuffers(1, &captureRBO);
 }
 
 OpenGLTextureCubemap::OpenGLTextureCubemap(GLuint id, int w, int h)
